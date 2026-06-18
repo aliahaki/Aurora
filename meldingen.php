5b260243fd69c839f34464b8aeabacd559afc1ed
aliahaki <341172@student.mboutrecht.nl>
2026-06-18 18:42:25 +0200

    Lege status voor conceptmeldingen toegevoegd

--- meldingen.php
+++ meldingen.php
@@ -250,8 +250,16 @@ try {
                                     <span style="font-size: 12px; color: #94a3b8;">Nog niet verzonden</span>
                                 </div>
                             </div>
+                             <?php 
+                        endif;
+                    endforeach; 
+                endif; 
+                
+                if (!$heeftConcepten): ?>
+                    <p style="color: #94a3b8; font-style: italic;">Er zijn momenteel geen concepten opgesteld.</p>
+                <?php endif; ?>
+            </div>
             
-        
             <hr style="border: 0; height: 1px; background: #e2e8f0; margin-bottom: 40px;">
 
        <div style="margin-bottom: 50px;">
